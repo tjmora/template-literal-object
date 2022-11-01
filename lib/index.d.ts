8f251982e475fd18bd4c94ff54b7bfacac6d079b
Tyrel Jonas Mora <tyreljonas.mora@gmail.com>
2022-11-02 07:46:08 +0800

    reverted module system

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export default class TLO {
-    private stringParts;
-    private interpolations;
-    constructor(stringParts: TemplateStringsArray, ...interpolations: any[]);
-    append(other: TLO): this;
-    parts(): [TemplateStringsArray, ...any[]];
-    static set(stringParts: TemplateStringsArray, ...interpolations: any[]): TLO;
-    static styled(stringParts: TemplateStringsArray, ...interpolations: (string | number | ((prop: any) => {}))[]): TLO;
-}
